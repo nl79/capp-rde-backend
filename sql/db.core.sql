c407e1c8bf869b2069824717511621f060858277
nl79 <nl79@njit.edu>
2015-02-18 16:40:33 -0500

    sql script updates

--- sql/db.core.sql
+++ sql/db.core.sql
@@ -1,17 +1,44 @@
--- Drop database rde_survey; 
--- CREATE database rde_survey; 
+/*
+select * 
+  from information_schema.columns 
+ where table_name = 'survey_question_table'; 
+*/
 
-drop table user_table; 
+Drop database rde_survey; 
+CREATE database rde_survey; 
+USE rde_survey; 
+ 
 CREATE TABLE user_table(
 entity_id	int		not null	PRIMARY KEY, 
 
 ); 
 
-DROP TABLE question_table; 
+CREATE TABLE question_type (
+	entity_id	int		not null	PRIMARY KEY, 
+	name		char(10)	not null
+); 
+ 
 CREATE TABLE question_table(
 	entity_id	int	not null	PRIMARY KEY, 
 	question	text	not null, 
-	q_type		enum('multi', 'single')	not null, 	-- controls if the questions are radio buttons or checkboxes
-	
-	
+	q_type		int not null FOREIGN KEY REFERENCES question_type(entity_id),	-- controls if the questions are radio buttons or checkboxes
+
+); 
+
+CREATE TABLE answer_table(
+	entity_id	int not null	PRIMARY KEY, 
+	q_id		int	not null	FOREIGN KEY REFERENCES question_table(entity_id), 
+	value		char(50)		not null 
+); 
+
+-- Survey table
+CREATE TABLE survey_table(
+	entity_id	int	not null	PRIMARY KEY, 
+	name		char(50)		not null, 
+	[description]	char(150)	null
+); 
+
+CREATE TABLE survey_question_table(
+	s_id		int not null FOREIGN KEY REFERENCES survey_table(entity_id), 
+	q_id		int	not null FOREIGN KEY REFERENCES question_table(entity_id)
 ); 
