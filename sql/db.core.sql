68e9eed88fa0bfcf78fa443553cb3baa37f97917
nl79 <nl79@njit.edu>
2015-02-18 15:39:20 -0500

    sql touch

--- sql/db.core.sql
+++ sql/db.core.sql
@@ -6,3 +6,12 @@ CREATE TABLE user_table(
 entity_id	int		not null	PRIMARY KEY, 
 
 ); 
+
+DROP TABLE question_table; 
+CREATE TABLE question_table(
+	entity_id	int	not null	PRIMARY KEY, 
+	question	text	not null, 
+	q_type		enum('multi', 'single')	not null, 	-- controls if the questions are radio buttons or checkboxes
+	
+	
+); 
